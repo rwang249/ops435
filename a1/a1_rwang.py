0a1e2df8e4145d339bdd7a60840949911f833398
rwang <you@example.com>
2021-01-06 22:55:39 -0500

    completed santize module

--- a1/a1_rwang.py
+++ a1/a1_rwang.py
@@ -40,8 +40,11 @@ def sanitize(obj1,obj2):
     and the 2nd string object contains letters that are allowed. This function will return the first object with 
     letters not in the 2nd string object removed.
     '''
-    ...
-    
+    results = ''
+    for char in obj1:
+        if char in obj2:
+            results = results + results.join(char)
+
     return results
 
 def size_check(obj, intobj):
@@ -88,32 +91,32 @@ if __name__ == "__main__":
    allow_chars = '0123456789'
    dob = sanitize(user_raw_data, allow_chars)
    print('Sanitized user data:', dob)
-   # setp 4
-   result = size_check(dob,8)
-   if result == False:
-       print("Error 09: wrong data entered")
-       sys.exit()
-   # step 5
-   year = int(dob[0:4])
-   month = int(dob[4:6])
-   day = int(dob[6:])
-   # step 6
-   result = range_check(year,(1900,9999))
-   if result == False:
-       print("Error 10: year out of range, must be 1900 or later")
-       sys.exit()
-   result = range_check(month,(1,12))
-   if result == False:
-       print("Error 02: Wrong month entered")
-       sys.exit()
-   result = leap_year(year)
-   if result == True:
-       days_in_month[2] = 29
-   result = range_check(day, (1, days_in_month[month]))
-   if result == False:
-       print("Error 03: wrong day entered")
-       sys.exit()
-   # step 7
-   new_dob = str(month_name[month - 1])+' '+ str(day)+', '+str(year)
-   # step 8
-   print("Your date of birth is:", new_dob)  
+#    # step 4
+#    result = size_check(dob,8)
+#    if result == False:
+#        print("Error 09: wrong data entered")
+#        sys.exit()
+#    # step 5
+#    year = int(dob[0:4])
+#    month = int(dob[4:6])
+#    day = int(dob[6:])
+#    # step 6
+#    result = range_check(year,(1900,9999))
+#    if result == False:
+#        print("Error 10: year out of range, must be 1900 or later")
+#        sys.exit()
+#    result = range_check(month,(1,12))
+#    if result == False:
+#        print("Error 02: Wrong month entered")
+#        sys.exit()
+#    result = leap_year(year)
+#    if result == True:
+#        days_in_month[2] = 29
+#    result = range_check(day, (1, days_in_month[month]))
+#    if result == False:
+#        print("Error 03: wrong day entered")
+#        sys.exit()
+#    # step 7
+#    new_dob = str(month_name[month - 1])+' '+ str(day)+', '+str(year)
+#    # step 8
+#    print("Your date of birth is:", new_dob)  
