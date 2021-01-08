a8d717a28bd3d60edef3d8f43bef110e99c3e3ca
rwang <you@example.com>
2021-01-07 22:45:59 -0500

    created assignment file with required modules

---
+++ a1/a1_b_rwang.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+''' template for ops435 assignment 1 B script
+-----------------------------------------------------------------------
+OPS435 Assignment 1 B - Fall 2020
+Program: a1_rwang.py 
+Author: Raphael Wang
+Done for personal learning. Only os, subprocess, and sys modules were allowed.
+Alternate assignment option
+'''
+import os
+import sys
+import subprocess
+
+def call_df():
+    '''
+    should take no arguments return a list of strings returned by the shell command.
+    '''
+
+    return output
+
+def call_free():
+    '''
+    should take no arguments, and should return a list of strings from the shell.
+    '''
+
+    return output
+
+def call_hostname():
+    '''
+    should take no arguments, and should return strings from the shell.
+    '''
+
+    return hostname
+
+def call_uptime():
+    '''
+    should take no arguments, and should return strings from the shell.
+    '''
+
+    return uptime
+
+def percent_to_graph(percent):
+    '''
+    will take an integer 'percent' and return a bar graph that represents this percentage. 
+    The bar graph should begin with '[', and end with ']'. Then contents inside should be 20 characters long.
+    '''
+
+    return output
+
+def print_percent_line(name, percent):
+    '''
+    will print a properly formatted line
+    '''
+
+    return output
