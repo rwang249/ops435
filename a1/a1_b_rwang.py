b7d49adf9e5b0d9e9b918c3c8f1a6d85059aaec1
rwang <you@example.com>
2021-01-08 01:00:36 -0500

    completed call_df and call_free modules

--- a1/a1_b_rwang.py
+++ a1/a1_b_rwang.py
@@ -14,16 +14,56 @@ import subprocess
 def call_df():
     '''
     should take no arguments return a list of strings returned by the shell command.
+    Output is filtered to omit any lines that contain loop or tmpfs. These are not proper file systems and should not be displayed.
     '''
+    process = subprocess.Popen(['df'], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    communicate = process.communicate()
+    output = communicate[0].decode('utf-8')
+    listOutput = output.split('\n')
+    count = 0
 
-    return output
+    # loop through listOutput and if file system is tmpfs or loop do not put into santized list for return
+    for string in listOutput:
+        fs = string.split(' ')
+        if fs[0] == 'tmpfs' or fs[0] == 'loop':
+            del listOutput[count]
+        count = count + 1
+    
+    return listOutput
 
 def call_free():
     '''
     should take no arguments, and should return a list of strings from the shell.
+    Will also calculate a percent of memory used divided by total memory.
     '''
+    # initialize list which will be returned by function
+    cleanedOutput = []
 
-    return output
+    # call subprocess to grab output of free, decode to utf-8 and split per line
+    process = subprocess.Popen(['free'], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    communicate = process.communicate()
+    output = communicate[0].decode('utf-8')
+    listOutput = output.split('\n')
+    memList = listOutput[1]
+    swapList = listOutput[2]
+
+    #split memory and swap lists per column and assign total and used ints for calculation
+    formattedMem = memList.split()
+    formattedSwap = swapList.split()
+    totalMem = formattedMem[1]
+    usedMem = formattedMem[2]
+    totalSwap = formattedSwap[1]
+    usedSwap = formattedSwap[2]
+
+    # calculate rounded percentage of memory used 
+    totalUsedMem = round((int(usedMem) / int(totalMem)) * 100)
+    totalUsedSwap = round((int(usedSwap) / int(totalSwap)) * 100)
+
+    # add rounded percentage to list for return
+    cleanedOutput.append(totalUsedMem)
+    cleanedOutput.append(totalUsedSwap)
+
+    return cleanedOutput
 
 def call_hostname():
     '''
@@ -53,3 +93,6 @@ def print_percent_line(name, percent):
     '''
 
     return output
+
+#call_df()
+#call_free()
