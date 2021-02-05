7bcf09d7341e5e56869b21871cbba6a4589ca0be
rwang <you@example.com>
2021-02-04 21:54:40 -0500

    completed all modules besides day_of_week. still need to add comments

--- a2/a2p_rwang.py
+++ a2/a2p_rwang.py
@@ -18,22 +18,58 @@ violators will be reported and appropriate action will be taken.
 
 class Date:
 
-    global days_in_month
-    days_in_month = {1:31, 2:28, 3:31, 4:30, 5:31, 6:30,
-                7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
-
-    def __init__(self, year, month, day):
+    # init
+    def __init__(self, year=1970, month=1, day=1):
+        # set dictionary to refer for each month
         global days_in_month
+        days_in_month = {1:31, 2:28, 3:31, 4:30, 5:31, 6:30,
+                    7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
+                    
         global date_in_days
         self.year = year
         self.month = month
         self.day = day
+
+    def __repr__(self):
+        return '%.4d-%.2d-%.2d' % (self.year, self.month, self.day)
+
+    def __str__(self):
+        return '%.4d/%.2d/%.2d' % (self.year, self.month, self.day)
+    
+    def __add__(self, day_increase):
+        addition_day = self.date_to_days() + day_increase
+        addition = self.days_to_time(addition_day)
+        return addition
+    
+    def __sub__(self, day_decrease):
+        subtraction_day = self.date_to_days() - day_decrease
+        subtraction = self.days_to_time(subtraction_day)
+        return subtraction
+
+    def tomorrow(self):
+        tomorrow_day = self.date_to_days() + 1
+        tomorrow = self.days_to_time(tomorrow_day)
+        return tomorrow
+    
+    def yesterday(self):
+        yesterday_day = self.date_to_days() - 1
+        yesterday = self.days_to_time(yesterday_day)
+        return yesterday
+    
+    def day_of_week(self):
+        day_of_week = days_in_month[self.month] - self.day
+        return day_of_week
+    
+    def date_to_days(self):
+        global date_in_days
         seconds = 0
         count = 1
         year_difference = self.year - 1970
-        year_calculation = self.year
+        year_calculation = 1970
 
+        #print('year difference:' + str(year_difference))
         while year_difference != 0:
+            #print(year_calculation)
             if (year_calculation % 4) == 0:
                 if (year_calculation % 100) == 0:
                     if (year_calculation % 400) == 0:
@@ -77,71 +113,100 @@ class Date:
                 seconds += days_in_month[count] * 86400
             count += 1
         
-        date_in_days = seconds / 86400
-
-
-    def __repr__(self):
-        return '%.4d-%.2d-%.2d' % (self.year, self.month, self.day)
-
-    def __str__(self):
-        return '%.4d/%.2d/%.2d' % (self.year, self.month, self.day)
-    
-    def __add__(day_increase):
-        # self.day = self.day + day_increase
-        # if self.day > days_in_month[self.month]:
-        #     self.month = self.month + 1
-        #     self.day = 1
-        #     if self.month > 12:
-        #         self.month == 1
-        #         self.year = self.year + 1
-
-        date_in_days += day_increase
-        
-        return self
-    
-    def __sub__(day_decrease):
-        # self.day = self.day - day_decrease
-        # if self.day < 1:
-        #     self.month = self.month - 1
-        #     self.day = days_in_month[self.month]
-        #     if self.month < 1:
-        #         self.month == 12
-        #         self.year = self.year - 1
-
-        return self
-
-    def tomorrow():
-
-        return tomorrow
-    
-    def yesterday():
-
-        return yesterday
-    
-    def day_of_week():
-
-        return day_of_week
+        date_in_days = (seconds / 86400)
+        return date_in_days
 
     def days_to_time(self, days):
+        date = Date(self.year, self.month, self.day)
         count = 1
-
-        #calculate number of years to add to epoch
-        num_of_years = days / 365
-
-        #calculate number of months to add to epoch
-        days_left = (days % 365)
+        #Start year calculation from epoch year
+        year_calculation = 1970
+        
         while True:
-            if count == self.month and days_left > 0 and days_left < 31:
-                days_left = (days % 365) - days_in_month[count]
+            #print(days)
+            if days < 0:
                 break
             else:
-                days_left -= days_in_month[count]
-            count += 1
+                if (year_calculation % 4) == 0:
+                    if (year_calculation % 100) == 0:
+                        if (year_calculation % 400) == 0:
+                            status = True
+                        else:
+                            status = False
+                    else:
+                        status = True
+                else:
+                    status = False
 
-        year_from_epoch
-        if 
-        month_from_epoch
-        days_from_epoch
+                if status == True:
+                    days_in_year = 366
+                    if days < days_in_year:
+                        break
+                    else:    
+                        days -= days_in_year
+                else:
+                    days_in_year = 365
+                    if days < days_in_year:
+                        break
+                    else:    
+                        days -= days_in_year
+                
+                if days != 0:
+                    year_calculation += 1
 
-        date = year_from_epoch + " - " + month_from_epoch + " - " + days_from_epoch
+        if status == True:
+            days_in_month[2] = 29
+        else:
+            days_in_month[2] = 28
+##################FIX CALCULATION###############        
+        month_count = 1
+        overflow_month = 0
+        overflow_year = 0
+        overflow_trigger = False
+        while days >= 0:
+            #print('days before calculation:' + str(days))
+            if month_count > 12:
+                overflow_trigger = True
+                month_count = 1
+                overflow_year += 1
+           # print('month:' + str(month_count))
+           # print('days in month: ' + str(days_in_month[month_count]))
+
+            if days == 0 and month_count == 1:
+                days = days_in_month[12]
+                month_count = 12
+                break
+            elif days <= days_in_month[month_count] and days != 0:
+               # print('break - after calculation:' + str(days))
+                break
+            else:
+                days -= days_in_month[month_count]
+              #  print('after calculation:' + str(days))
+                month_count += 1
+                if overflow_trigger == True:
+                    overflow_month += 1
+
+        print(year_calculation)
+        #print(month_count)
+        #print('overflow info:' + str(overflow_month) + str(overflow_year) + str(overflow_trigger))
+        date.year = year_calculation + overflow_year
+        date.month = month_count + overflow_month
+        date.day = days
+        
+       # print('info' + str(date.year) + ' ' +  str(date.month) + ' ' + str(date.day))
+        if date.day > days_in_month[date.month]:
+            date.month += 1
+            date.day = 1
+           # print(date.month)
+            if date.month > 12:
+                date.month == 1
+                date_year += 1
+        elif date.day < 1:
+           # print(date.month)
+            date.month +- 1
+            date.day = days_in_month[date.month]
+           # print(date.day)
+            if date.month < 1:
+                date.month == 12
+                date.year -= 1
         return date
